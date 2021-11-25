6f996455c0ed3aed28a5cfa9e9fc97676a4ba374
Aayush's Mac <SuperAayush@1234s-MacBook-Air.local>
2021-11-25 23:19:08 +0530

    Login Page Finished

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,15 +1,59 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "../Styling/Login.css"
-import img from "../Images/Logo.png"
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Header_Login from "./Header_Login"
+import twitter from "../Images/twitter.png"
+import github from "../Images/github.png"
+import facebook from "../Images/facebook.png"
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
     return (
-        <div className="container1">
-            <h1>Hello</h1>
-            <img className="logo" src={img} alt="" />
+      <div className="container_body">
+        <Header_Login />
+          <div className="Login">
+            <Form onSubmit={handleSubmit}>
+              <Form.Group size="lg" controlId="email">
+                <Form.Label className="username">Username or Email</Form.Label>
+                <Form.Control
+                  autoFocus
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="input_1"
+                />
+              </Form.Group>
+              <Form.Group size="lg" controlId="password">
+                <Form.Label className="pass">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input_2"
+                />
+              </Form.Group>
+              <Button className="login_btn" block size="lg" type="submit" disabled={!validateForm()}>
+                Login
+              </Button>
+          </Form>
+          <Button className="twitter"><img className="twitter_logo" src={twitter} alt="" srcset="" /> Log In with Twitter</Button>
+          <Button className="github"><img className="github_logo" src={github} alt="" srcset="" />Log In with GitHub</Button>
+          <Button className="facebook"><img className="facebook_logo" src={facebook} alt="" srcset="" />Log In with Facebook</Button>
+          </div>
         </div>
-    )
-}
+    );
+  }
 
 
 export default Login
