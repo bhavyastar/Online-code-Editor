682fd996d308c4f634469612d2e7c1e60be3e855
kanika <kanika@example.com>
2021-11-28 21:36:12 +0530

    Videos area final done

--- src/Components/Testing1.js
+++ src/Components/Testing1.js
@@ -2,24 +2,18 @@ import React from 'react'
 import img from "../Images/Logo.png"
 import "./testing1.css"
 import ReactPlayer from "react-player";
-
-
-
-
 function Testing1(props) {
     return (
-      
-
-     
-        <div className="testing1">
+      <div className="testing1">
             <div className="lines">
                 <h1 className="first">Wanna do coding together</h1>
                 <h3 className="second">Get started here</h3>
             </div>
+            <div className="videos">
             <div className="video_player_1">
             <ReactPlayer
-            width={"450px"}
-            height={"400px"}
+            width={"400px"}
+            height={"300px"}
   url='https://youtu.be/1rTaNzjGOek'
   playing
   muted
@@ -33,7 +27,7 @@ function Testing1(props) {
 <div className="video_player_2">
 <ReactPlayer
 width={"400px"}
-height={"400px"}
+height={"300px"}
   url='https://youtu.be/S8yn3-WpVV8'
   playing
   muted
@@ -47,7 +41,7 @@ height={"400px"}
 <div className="video_player_3">
 <ReactPlayer
 width={"400px"}
-height={"400px"}
+height={"300px"}
   url='https://youtu.be/hQAHSlTtcmY'
   playing
   muted
@@ -58,7 +52,7 @@ height={"400px"}
     
   }}}}
 />
-</div>
+</div></div>
             </div>
             
         </div>
