07e8b6fae65c82b340578481fdef6a29ee0602a8
Bhavya <bhavya.sachdeva.58@gmail.com>
2021-11-28 23:24:02 +0530

    Text-Animation Added

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -7,21 +7,10 @@ import "../Styling/Homepage.css"
  const Homepage = () => {
      return (
      <div> 
-     <div class="words word-1">
-   <button className="unique">  
-  <span>C</span>
-  <span>o</span>
-  <span>d</span>
-  <span>e</span>
-  <span>Y</span>
-  <span>o</span>
-  <span>u</span>
-  <span>r</span>
-  <span>W</span>
-  <span>a</span>
-  <span>y</span>
-  </button>
-
+     <div class="wrapper">
+    <div class="typing-demo">
+      CodeYourWay
+    </div>
 </div>
 <button className="login1">
   Login
