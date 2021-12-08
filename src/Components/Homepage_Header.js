6878eb0369a8016bae53f6ee05a914156ec6ea8a
Jaideep2912 <jaideep2912@gmail.com>
2021-12-08 23:01:32 +0530

    Files Added

---
+++ src/Components/Homepage_Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../Styling/Homepage_Header.css"
+import img from "../Images/Logo.png"
+import github2 from "../Images/github2.png"
+const Homepage_Header = () =>{
+return(
+     
+      <div className="container1">
+          <img className="image" src={img} alt=""/>
+             <div className="head">
+                 </div>
+              <div className="para">
+                  <p className="about">About Us</p>
+                  <p className="help">Help</p>
+                 <img className="git" src={github2}/>
+                  </div>
+          </div>
+    
+
+)
+}
+export default Homepage_Header
