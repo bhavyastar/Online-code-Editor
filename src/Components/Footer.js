7212bf24a19c43c9d2e972605933bb4b846734b4
Jaideep2912 <jaideep2912@gmail.com>
2021-12-12 00:58:42 +0530

    Footer Added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,17 +7,30 @@ const Footer = () =>{
     
         <div className='footer'>
             <h1 className="head"> Let's Get Started </h1>
-            <div className="line"><hr></hr></div>
+            <div className="line">
+                <hr className="line_a"></hr>
+                <hr className="line_b"></hr>
+                <hr className="line_c"></hr>
+                </div>
             <div className="middle">
-                
+                <h1 className="heading">CODEYOURWAY</h1>
+                <vr className="line_0"></vr>
             </div>
             <div className="below">
                 <h1>Back To Top </h1>
 
             </div>
-            <div className="line2"><hr></hr></div>
+            <div className="line2">
+                <hr className="line_2a"></hr>
+                <hr className="line_2b"></hr>
+                <hr className="line_2c"></hr>
+                </div>
             <div className="bottom">
-                <p className="items">Languages Country Privacy Support </p>
+                <p className="item1">Country</p>
+                <p className="item2">Languages</p>
+                <p className="item3">Legal Terms</p>
+                <p className="item4">Privacy Policy</p>
+                <p className="item5">Report</p>
             </div>
             
           
